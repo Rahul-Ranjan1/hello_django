f99bf9e921a52f4e69d7864ca00ef817165f952c
RAHUL RANJAN <rajeshrahulranjan@gmail.com>
2020-03-13 11:58:20 +0530

    Added files and changes required for deployment to heroku

--- MyBlogger/settings.py
+++ MyBlogger/settings.py
@@ -20,10 +20,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'oe2lae7szf$tvr&awqtwr69^op$almf%f3tguk-@6k2z!4k8__'
+#SECRET_KEY = 'oe2lae7szf$tvr&awqtwr69^op$almf%f3tguk-@6k2z!4k8__'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+#DEBUG = True
+
+
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'oe2lae7szf$tvr&awqtwr69^op$almf%f3tguk-@6k2z!4k8__')
+
+DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = []
 
@@ -42,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -133,3 +139,11 @@ AUTHENTICATION = 'plain'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = "/media/"
 
+# Heroku: Update database configuration from $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
